f15b79e03da3c890052ba70ca71d42b28f3ab426
vikasdesale <vikas.desaleu@gmail.com>
2017-12-27 21:58:02 +0530

    String_datatype_and_concatination #48

---
+++ 2. Introduction to Javascript/4.Quiz5_String.js
@@ -0,0 +1,7 @@
+//String Datatype
+console.log("String");
+console.log('String');
+//String Concatination
+console.log("hello" + "world");
+console.log("Hello + 5*10");
+console.log( "Hello" + 5*10);
