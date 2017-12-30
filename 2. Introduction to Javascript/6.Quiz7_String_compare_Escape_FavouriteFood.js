a56b0da38b89308c0d1a95f58b1ac11a04496af9
vikasdesale <vikas.desaleu@gmail.com>
2017-12-30 22:21:00 +0530

    fix: #52 6.Quiz7_String_compare_Escape_FavouriteFood

---
+++ 2. Introduction to Javascript/6.Quiz7_String_compare_Escape_FavouriteFood.js
@@ -0,0 +1,35 @@
+//String index
+var quote = "Stay awhile and listen!";
+console.log(quote[6]);
+
+//Escape Char
+//"The file located at "C:\\Desktop\My Documents\Roster\names.txt" contains the names on the roster."
+
+//Ans: "The file located at \"C:\\\\Desktop\\My Documents\\Roster\\names.txt\" contains the names on the roster."
+
+
+//Comparing
+
+//"Yes" == "yes"
+//->false
+//case sensitive
+//'Y' != 'y'
+//true
+
+//Enter each expression into the console. Check the ones that evaluate to true.
+
+//"green" == "blue"
+//->"green" == "green"
+//"green" == "Green"
+//"Green" == "green"
+//"green" > "blue"
+//->"green" > "green"
+//"green" > "Green"
+//"Green" > "green"
+
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ */
+
+FavoriteFood="Pizza";
+console.log(FavoriteFood);
