3850eeb8ca26235f3a7a1611b67a8715f0a605fc
vikasdesale <vikas.desaleu@gmail.com>
2017-12-31 22:23:16 +0530

    fix: #58 String_concate_semicolon_and_datatypes

---
+++ 2. Introduction to Javascript/8.Quiz10_20.js
@@ -0,0 +1,39 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+var haiku = "Blowing from the west"+"\nFallen leaves gather"+"\nIn the east.";
+console.log(haiku);
+/*Submit to check your answer choices!
+LETTER
+TYPE
+A - Udacity
+
+String
+
+B - Meet Christian Plagemann, team lead for the new VR Developer Nanodegree program at Udacity! Here he is introducing and describing our latest offering
+
+String
+
+C - blog.udacity.com
+
+String
+
+D - Whether something was "liked" or not.
+
+Boolean
+
+E - 80
+
+Number
+
+*/
+
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// your code goes here
+ var thingOne="red";
+ var thingTwo="blue";
+ console.log(thingOne+thingTwo);
