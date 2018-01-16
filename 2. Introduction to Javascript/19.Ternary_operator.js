d0146551564da9fa6904af8da1b90a2deb662dd7
vikasdesale <vikas.desaleu@gmail.com>
2018-01-16 23:52:47 +0530

    fix: #80 19.Ternary_operator

---
+++ 2. Introduction to Javascript/19.Ternary_operator.js
@@ -0,0 +1,9 @@
+
+var isGoing = true;
+var color = isGoing ? "green" : "red";
+console.log(color);
+
+var adult = true;
+var preorder = true;
+
+console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "the gate") + ".");
